812d7f1a9e8ad45fc9f15094af1e626419f5fd70
thcomp <thcomp0520@gmail.com>
2017-03-01 23:19:23 +0900

    一時保存

--- glhelper/src/main/java/jp/co/thcomp/glsurfaceview/GLElementUpdater.java
+++ glhelper/src/main/java/jp/co/thcomp/glsurfaceview/GLElementUpdater.java
@@ -3,151 +3,154 @@ package jp.co.thcomp.glsurfaceview;
 import java.util.Comparator;
 import java.util.List;
 
+import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Message;
 import android.os.SystemClock;
 
 import jp.co.thcomp.util.HashMultimap;
-import jp.co.thcomp.util.LooperThread;
 
 public class GLElementUpdater {
-	public static final int NO_MORE_ANIMATION = -1;
+    public static final int NO_MORE_ANIMATION = -1;
 
-	protected static final int MIN_POLLING_INTERVAL = 10;
-	protected GLDrawView mView;
-	protected Object mSemapho = new Object();
-	protected boolean mResumed = false;
-	protected HandlerThread mUpdaterThread;
-	protected LooperThread.Runnable mUpdaterRunnable;
-	protected HashMultimap<Integer, GLDrawElement> mUpdateElementMap = new HashMultimap<Integer, GLDrawElement>();
+    protected static final int NOTIFY_ADD_ELEMENT = 1;
+    protected static final int MIN_POLLING_INTERVAL = 10;
+    protected GLDrawView mView;
+    protected Object mSemapho = new Object();
+    protected boolean mResumed = false;
+    protected HandlerThread mUpdaterThread;
+    protected Handler mUpdaterHandler;
+    protected HashMultimap<Integer, GLDrawElement> mUpdateElementMap = new HashMultimap<Integer, GLDrawElement>();
 
-	public GLElementUpdater(GLDrawView view){
-		mView = view;
-		mUpdaterRunnable = new LooperThread.Runnable() {
-			public Object run(Object param) {
-				return updateDrawElement(param);
-			}
-		};
-		mUpdaterThread = new LooperThread(mUpdaterRunnable, null);
-	}
+    public GLElementUpdater(GLDrawView view) {
+        if (view == null) {
+            throw new NullPointerException("view == null");
+        }
+        mView = view;
+        mUpdaterThread = new HandlerThread(GLElementUpdater.class.getSimpleName(), Thread.MIN_PRIORITY);
+        mUpdaterHandler = new Handler(mUpdaterThread.getLooper(), mUpdateDrawElementCallback);
+    }
 
-	public void add(GLDrawElement drawElement){
-		synchronized(mSemapho){
-			mUpdateElementMap.add(0, drawElement);
-			mUpdaterThread.startRunnable();
-		}
-	}
+    public void add(GLDrawElement drawElement) {
+        synchronized (mSemapho) {
+            mUpdateElementMap.add(0, drawElement);
+            mUpdaterHandler.sendEmptyMessage(NOTIFY_ADD_ELEMENT);
+        }
+    }
 
-	public void remove(GLDrawElement drawElement){
-		synchronized(mSemapho){
-			mUpdateElementMap.removeValue(drawElement);
-		}
-	}
+    public void remove(GLDrawElement drawElement) {
+        synchronized (mSemapho) {
+            mUpdateElementMap.removeValue(drawElement);
+        }
+    }
 
-	public void removeAll(){
-		synchronized(mSemapho){
-			mUpdateElementMap.clearAll();
-		}
-	}
+    public void removeAll() {
+        synchronized (mSemapho) {
+            mUpdateElementMap.clearAll();
+        }
+    }
 
-	public void onResume() {
-		mResumed = true;
-		if(mUpdateElementMap.size() > 0){
-			mUpdaterThread.startRunnable();
-		}
-	}
+    public void onResume() {
+        mResumed = true;
+        if (mUpdateElementMap.size() > 0) {
+            mUpdaterHandler.sendEmptyMessage(NOTIFY_ADD_ELEMENT);
+        }
+    }
 
-	public void onPause() {
-		mResumed = false;
-		if(mUpdateElementMap.size() > 0){
-			synchronized(mUpdateElementMap){
-				mUpdateElementMap.notify();
-			}
-			mUpdaterThread.stopRunnable();
-		}
-	}
+    public void onPause() {
+        mResumed = false;
+        if (mUpdateElementMap.size() > 0) {
+            mUpdaterHandler.removeMessages(NOTIFY_ADD_ELEMENT);
+        }
+    }
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public Object updateDrawElement(Object param){
-		GLDrawElement drawElement = null;
-		List<Integer> sleepIntervalList = null;
-		List<GLDrawElement> tempElementList = null;
-		int sleepInterval = 0;
-		int nextSleepInterval = -1;
-		int targetSleepInterval = 0;
-		long currentTime1 = 0;
-		long currentTime2 = 0;
-		boolean needRender = false;
+    private Handler.Callback mUpdateDrawElementCallback = new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message message) {
+            boolean ret = false;
 
-		synchronized(mSemapho){
-			while(mResumed && mUpdateElementMap.size()>0){
-				currentTime1 = SystemClock.elapsedRealtime();
-				mUpdateElementMap.sort(new Comparator(){
-					public int compare(Object object1, Object object2) {
-						int ret = 0;
+            if(message.what == NOTIFY_ADD_ELEMENT){
+                ret = true;
 
-						if((object1 instanceof Integer) && (object2 instanceof Integer)){
-							Integer i1 = (Integer)object1;
-							Integer i2 = (Integer)object2;
+                GLDrawElement drawElement = null;
+                List<Integer> sleepIntervalList = null;
+                List<GLDrawElement> tempElementList = null;
+                int sleepInterval = 0;
+                int nextSleepInterval = -1;
+                int targetSleepInterval = 0;
+                long currentTime1 = 0;
+                long currentTime2 = 0;
+                boolean needRender = false;
 
-							ret = (i1 == i2) ? 0 : i1 < i2 ? -1 : 1;
-						}
-		
-						return ret;
-					}
-				});
-				sleepIntervalList = mUpdateElementMap.keyList();
+                synchronized (mSemapho) {
+                    if (mResumed && mUpdateElementMap.size() > 0) {
+                        currentTime1 = SystemClock.elapsedRealtime();
+                        mUpdateElementMap.sort(new Comparator() {
+                            public int compare(Object object1, Object object2) {
+                                int ret = 0;
 
-				needRender = false;
-				for(int i=0, size=sleepIntervalList.size(); i<size; i++){
-					targetSleepInterval = sleepIntervalList.get(i);
-					tempElementList = mUpdateElementMap.remove(targetSleepInterval);
+                                if ((object1 instanceof Integer) && (object2 instanceof Integer)) {
+                                    Integer i1 = (Integer) object1;
+                                    Integer i2 = (Integer) object2;
 
-					if(targetSleepInterval == 0){
-						nextSleepInterval = -1;
-						for(int j=0, sizeJ=tempElementList.size(); j<sizeJ; j++){
-							drawElement = tempElementList.get(j);
-							if(drawElement != null){
-								if((nextSleepInterval = drawElement.update()) < 0){
-									mUpdateElementMap.removeValue(drawElement);
-								}else{
-									nextSleepInterval = nextSleepInterval - (nextSleepInterval % MIN_POLLING_INTERVAL);
-									if(sleepInterval > nextSleepInterval){
-										sleepInterval = nextSleepInterval;
-									}
+                                    ret = (i1 == i2) ? 0 : i1 < i2 ? -1 : 1;
+                                }
 
-									// add to sleep+MIN_POLLING_INTERVAL, because over 0 lists will move to current - MIN_POLLING_INTERVAL
-									mUpdateElementMap.add(nextSleepInterval + MIN_POLLING_INTERVAL, drawElement);
-								}
-							}
-						}
-						needRender = true;
-					}else{
-						targetSleepInterval -= MIN_POLLING_INTERVAL;
-						mUpdateElementMap.addAll(targetSleepInterval, tempElementList);
-					}
-				}
+                                return ret;
+                            }
+                        });
+                        sleepIntervalList = mUpdateElementMap.keyList();
 
-				if(needRender){
-					mView.requestRender();
-				}
-				try {
-					currentTime2 = SystemClock.elapsedRealtime();
-					sleepInterval = sleepInterval - (int)(currentTime2 - currentTime1);
-					if(sleepInterval > 0){
-						synchronized(mUpdateElementMap){
-							mUpdateElementMap.wait(sleepInterval);
-						}
-					}
-				} catch (InterruptedException e) {
-					break;
-				}
-			}
-		}
+                        needRender = false;
+                        for (int i = 0, size = sleepIntervalList.size(); i < size; i++) {
+                            targetSleepInterval = sleepIntervalList.get(i);
+                            tempElementList = mUpdateElementMap.remove(targetSleepInterval);
 
-		return null;
-	}
+                            if (targetSleepInterval == 0) {
+                                nextSleepInterval = -1;
+                                for (int j = 0, sizeJ = tempElementList.size(); j < sizeJ; j++) {
+                                    drawElement = tempElementList.get(j);
+                                    if (drawElement != null) {
+                                        if ((nextSleepInterval = drawElement.update()) < 0) {
+                                            mUpdateElementMap.removeValue(drawElement);
+                                        } else {
+                                            nextSleepInterval = nextSleepInterval - (nextSleepInterval % MIN_POLLING_INTERVAL);
+                                            if (sleepInterval > nextSleepInterval) {
+                                                sleepInterval = nextSleepInterval;
+                                            }
 
-	public interface OnUpdateListener{
-		int onUpdate(GLDrawElement drawElement);
-	}
+                                            // add to sleep+MIN_POLLING_INTERVAL, because over 0 lists will move to current - MIN_POLLING_INTERVAL
+                                            mUpdateElementMap.add(nextSleepInterval + MIN_POLLING_INTERVAL, drawElement);
+                                        }
+                                    }
+                                }
+                                needRender = true;
+                            } else {
+                                targetSleepInterval -= MIN_POLLING_INTERVAL;
+                                mUpdateElementMap.addAll(targetSleepInterval, tempElementList);
+                            }
+                        }
+
+                        if (needRender) {
+                            mView.requestRender();
+                        }
+
+                        currentTime2 = SystemClock.elapsedRealtime();
+                        sleepInterval = sleepInterval - (int) (currentTime2 - currentTime1);
+                        if (sleepInterval > 0) {
+                            mUpdaterHandler.sendEmptyMessageDelayed(NOTIFY_ADD_ELEMENT, sleepInterval);
+                        }else{
+                            mUpdaterHandler.sendEmptyMessage(NOTIFY_ADD_ELEMENT);
+                        }
+                    }
+                }
+            }
+
+            return ret;
+        }
+    };
+
+    public interface OnUpdateListener {
+        int onUpdate(GLDrawElement drawElement);
+    }
 }
